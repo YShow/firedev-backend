d93d713a17662f7aa7a4190b474c4b70b548ead9
Yasser Bazzi <yassermbb@gmail.com>
2022-10-07 16:43:20 -0300

    primeiro

---
+++ README.md
@@ -0,0 +1,9 @@
+buildar projeto: docker compose --build -d
+
+parar projetos: docker kill $(docker ps -q)
+
+versao java: 17
+
+IDE: eclipse ou intelij
+
+externo para interno
---
+++ firedevback/src/test/java/com/firedev/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firedev.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
